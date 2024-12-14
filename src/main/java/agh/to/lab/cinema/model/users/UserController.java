d2fce530c1c7c27b422e4019a424728e96637dac
pajka2T <tom16lolo@gmail.com>
2024-12-14 11:31:29 +0100

    Register and login endpoints and controllers modified.

--- src/main/java/agh/to/lab/cinema/model/users/UserController.java
+++ src/main/java/agh/to/lab/cinema/model/users/UserController.java
@@ -2,9 +2,13 @@ package agh.to.lab.cinema.model.users;
 
 import agh.to.lab.cinema.model.roles.RoleType;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
@@ -30,38 +34,38 @@ public class UserController {
     }
 
     @PostMapping(value = "/register", consumes = "application/json")
-    public String registerUser(@RequestBody CinemaUser user) {
+    public ResponseEntity<Optional<CinemaUser>> registerUser(@RequestBody CinemaUser user) {
         try {
             System.out.println(user);
             if (!CinemaUser.validateEmail(user)) {
-                return "Invalid email";
+                return new ResponseEntity<>(Optional.empty(), HttpStatus.valueOf(401));
             }
             CinemaUser createdUser = userService.createUser(user.getUsername(), user.getPassword(), user.getEmail(), RoleType.USER);
             if (createdUser.getUsername().equals("admin")
                     && createdUser.getEmail().equals("admin@agh.edu.pl"))
                 createdUser.setRole(userService.getAdminRole());
             userService.addUser(createdUser);
-            return "User added";
+            return new ResponseEntity<>(Optional.of(createdUser), HttpStatus.OK);
         } catch (DataIntegrityViolationException e) {
             System.out.println(e.getMessage().contains("username") + " " + e.getMessage().contains("email"));
             if (e.getMessage().contains("USERNAME NULLS FIRST"))
-                return "Username not available";
+                return new ResponseEntity<>(Optional.empty(), HttpStatus.valueOf(402));
             if (e.getMessage().contains("EMAIL NULLS FIRST"))
-                return "Email occupied";
-            return e.getMessage();
+                return new ResponseEntity<>(Optional.empty(), HttpStatus.valueOf(403));
+            return new ResponseEntity<>(Optional.empty(), HttpStatus.valueOf(404));  //e.getMessage()
         }
     }
 
     @PostMapping(value = "/login", consumes = "application/json")
-    public String loginUser(@RequestBody CinemaUser user) {
+    public ResponseEntity<Optional<CinemaUser>> loginUser(@RequestBody CinemaUser user) {
         List<CinemaUser> users = userService.getUsers();
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         for (CinemaUser u : users) {
             if (u.getUsername().equals(user.getUsername()) && passwordEncoder.matches(user.getPassword(), u.getPassword())) {
-                return "User logged in";
+                return new ResponseEntity<>(Optional.of(u), HttpStatus.OK);
             }
         }
-        return "User not found";
+        return new ResponseEntity<>(Optional.empty(), HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/delete/{id}")
