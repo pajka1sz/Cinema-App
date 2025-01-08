d73c3365b5d493c43fca3c2cd67ec6247fd195bd
lachimex <dydmichal@gmail.com>
2025-01-08 08:30:11 +0100

    reloading view after reservation

--- src/main/java/agh/to/lab/cinema/viewController/user/UserSeancesController.java
+++ src/main/java/agh/to/lab/cinema/viewController/user/UserSeancesController.java
@@ -129,6 +129,7 @@ public class UserSeancesController {
                     try {
                         HttpResponse<String> reserveResponse = reserveClient.send(reserveRequest, HttpResponse.BodyHandlers.ofString());
                         System.out.println(reserveResponse.body());
+                        CinemaApp.loadView("views/user/movieDetails.fxml");
                     } catch (IOException | InterruptedException e) {
                         e.printStackTrace();
                     }
