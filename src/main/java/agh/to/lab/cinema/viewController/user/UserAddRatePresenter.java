c9c95aef13c3e52f186c69f0b5ea343bac006286
pajka2T <tom16lolo@gmail.com>
2025-01-20 13:27:25 +0100

    Handling errors added to adding rate dialog stage.

--- src/main/java/agh/to/lab/cinema/viewController/user/UserAddRatePresenter.java
+++ src/main/java/agh/to/lab/cinema/viewController/user/UserAddRatePresenter.java
@@ -4,9 +4,13 @@ import agh.to.lab.cinema.model.movies.Movie;
 import agh.to.lab.cinema.model.users.CinemaUser;
 import agh.to.lab.cinema.restController.MovieRateController;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.springframework.http.HttpStatus;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -23,6 +27,9 @@ public class UserAddRatePresenter {
     private TextField rateRateTextField;
     @FXML
     private TextField rateDescriptionTextField;
+    @FXML
+    private Label rateDialogInfoLabel;
+    private BooleanProperty labelVisible = new SimpleBooleanProperty(false);
 
     public UserAddRatePresenter(Movie movie, CinemaUser user) {
         this.movie = movie;
@@ -45,11 +52,20 @@ public class UserAddRatePresenter {
 
         rateMovieTitleTextField.setText(movie.getTitle());
         rateMovieTitleTextField.setEditable(false);
+        rateDialogInfoLabel.visibleProperty().bind(labelVisible);
+
+        rateRateTextField.textProperty().addListener((obj, oldVal, newVal)
+                -> labelVisible.setValue(newVal.trim().equals(oldVal.trim())));
+        rateDescriptionTextField.textProperty().addListener((obj, oldVal, newVal)
+                -> labelVisible.setValue(newVal.trim().equals(oldVal.trim())));
     }
 
     @FXML
     private void handleOkAction() {
         try {
+            if (rateRateTextField.getText().isEmpty())
+                throw new RuntimeException("Rate field must not be empty!");
+
             HttpClient client = HttpClient.newHttpClient();
             String url = MovieRateController.getBaseUrl() + "/add";
             HttpRequest request = HttpRequest.newBuilder()
@@ -63,9 +79,24 @@ public class UserAddRatePresenter {
                             .toString()))
                     .build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
 
-            stage.close();
+            if (response.statusCode() == HttpStatus.OK.value()) {
+                labelVisible.setValue(false);
+                stage.close();
+            }
+            if (response.statusCode() == HttpStatus.BAD_REQUEST.value()) {
+                labelVisible.setValue(true);
+                rateDialogInfoLabel.setText("You have already given a rate for this film!");
+            }
+            else if (response.statusCode() == HttpStatus.NOT_FOUND.value()) {
+                labelVisible.setValue(true);
+                rateDialogInfoLabel.setText("You have not watched this film yet!");
+            }
+            System.out.println(response.body());
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            labelVisible.setValue(true);
+            rateDialogInfoLabel.setText("Rate field must not be empty!");
         } catch (Exception e) {
             e.printStackTrace();
         }
